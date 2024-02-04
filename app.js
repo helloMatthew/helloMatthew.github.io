61015b0e3d50054b0136a4446495c991a67545c9
mattheweq <repos@mattheweq.com>
2024-02-04 02:00:43 +0000

    add div/flex to category profile icon/text to tighen up alignment

--- app.js
+++ app.js
@@ -101,8 +101,8 @@ function createCard(item, index) {
     categoryAnchorElement.setAttribute('data-type', 'category');
     categoryAnchorElement.href = '#' + categoryText.toLowerCase(); 
     
-    categoryAnchorElement.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="mr-2 inline-block w-5 h-5">
-  <path fill-rule="evenodd" d="M18.685 19.097A9.723 9.723 0 0 0 21.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 0 0 3.065 7.097A9.716 9.716 0 0 0 12 21.75a9.716 9.716 0 0 0 6.685-2.653Zm-12.54-1.285A7.486 7.486 0 0 1 12 15a7.486 7.486 0 0 1 5.855 2.812A8.224 8.224 0 0 1 12 20.25a8.224 8.224 0 0 1-5.855-2.438ZM15.75 9a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" clip-rule="evenodd" /></svg><span class="mt-2">${categoryText}</span>`;
+    categoryAnchorElement.innerHTML = `<div class="flex"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="mr-2 inline-block w-5 h-5">
+  <path fill-rule="evenodd" d="M18.685 19.097A9.723 9.723 0 0 0 21.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 0 0 3.065 7.097A9.716 9.716 0 0 0 12 21.75a9.716 9.716 0 0 0 6.685-2.653Zm-12.54-1.285A7.486 7.486 0 0 1 12 15a7.486 7.486 0 0 1 5.855 2.812A8.224 8.224 0 0 1 12 20.25a8.224 8.224 0 0 1-5.855-2.438ZM15.75 9a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" clip-rule="evenodd" /></svg><span>${categoryText}</span></div>`;
     categoryAnchorElement.classList.add('filter', 'category-item', 'text-sm', 'inline-block', 'mr-2', 'mt-4', 'mb-2', 'font-normal', 'text-[#403FFC99]', 'hover:text-[#C689BF]');
 
     card.appendChild(categoryAnchorElement);
